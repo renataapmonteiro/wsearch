a1733b66c8ec081b49e035eb4240c3f69af6d43e
renataapmonteiro <60657822+renataapmonteiro@users.noreply.github.com>
2020-06-11 08:44:33 -0300

    second commit: start stage of the app

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class WikipediaService {
+  search(term:string){
+    return "hello there"
+  }
 
   constructor() { }
 }
